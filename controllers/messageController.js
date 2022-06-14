9109c75205793a64b1671cc78533a8162a1f0a37
tchoforwilson <tchoforwilson@gmail.com>
2022-06-14 14:30:17 +0100

    created method setMessageFaculty

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,6 +1,11 @@
 import Message from "../models/MessageModel.js";
 import * as factory from "./handleFactory.js";
 
+export const setMessageFaculty = (req, res, next) => {
+  if (!req.body.faculty) req.body.faculty = req.params.facultyId;
+  next();
+};
+
 export const createMessage = factory.createOne(Message);
 export const getMessage = factory.getOne(Message);
 export const getAllMessage = factory.getAll(Message);
