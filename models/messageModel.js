083c1365d24c046de47baf116e4559f3f492ce2d
tchoforwilson <tchoforwilson@gmail.com>
2022-06-14 14:24:07 +0100

    Message model

---
+++ models/messageModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    contact: {
+      type: String,
+      required: [true, "Contact required!!"],
+      trim: true,
+    },
+    subject: {
+      type: String,
+      required: [true, "Subject required!!"],
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: [true, "Message required!!"],
+      trim: true,
+    },
+    faculty: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Faculty",
+      required: [true, "message must belong to a faculty."],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
