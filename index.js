578e2bf60c212ad0a8690707fde5eaa5e3c2ab23
tchoforwilson <tchoforwilson@gmail.com>
2022-06-14 15:44:02 +0100

    index file

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import connectDB from "./config/db.js";
 // connect to Database
 connectDB();
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 9000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}....`);
 });
